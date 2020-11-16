1c97571dfffa7eba61e840e2979be8cadeeb78c8
Kioskpeda <alle_yeah@live.se>
2020-11-16 12:36:28 +0100

    For loop with Simon

--- src/main/java/Data/People.java
+++ src/main/java/Data/People.java
@@ -3,15 +3,33 @@ package Data;
 import model.Person;
 
 public class People  {
-    private static Person[] Person;
+
+
+    private static Person[] people = new Person[0];
+
     public int size(){
-        return size();
+        return people.length;
     }
     public Person[] findAll(){
-        return findAll();
+        return people;
     }
-    public Person[] findById(int personId){
-        return findById(personId);
+    //Created with teacher.
+
+    public Person findById(int personId){
+        //Iterate through array
+        //Check if personId is the same as personId in array.
+        //return found person
+
+        for(int i =0; i< people.length; i++){
+            if(personId == people[i].getPersonId()){
+                return people[i];
+            }
+        }
+        return null;
+    }
+
+    public void clear(){
+
     }
 
 
