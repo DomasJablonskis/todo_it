54418407fedbf29540bade0b2750b3bb0b37b214
Kioskpeda <alle_yeah@live.se>
2020-11-17 19:29:13 +0100

    9 - Trying to replicate Person class with Todo.

--- src/main/java/Data/TodoItems.java
+++ src/main/java/Data/TodoItems.java
@@ -1,5 +1,31 @@
 package Data;
 
-public class TodoItems {
+import model.Todo;
 
+public class TodoItems {
+    private static TodoItems[] items = new TodoItems[]{
+    public int size(){
+        return items.length;
+    }
+    public TodoItems[] findAll(){
+        return items;
+    }
+
+
+    public TodoItems findById(int itemsId){
+        //Iterate through array
+        //Check if personId is the same as personId in array.
+        //return found person
+
+        for(int i =0; i< items.length; i++){
+            if(itemsId == items[i].getItemsId()){
+                return items[i];
+            }
+        }
+        return null;
+    }
+
+    public void clear(){
+
+    }
 }
